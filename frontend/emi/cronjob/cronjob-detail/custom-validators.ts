dfb80d01babffb5202aa6629832bdfa5341bf2c5
Daniel Tobon <daniel.tobon@nebulae.com.co>
2018-05-25 14:56:14 -0500

    Se elimina cron-parser ya que no se requiere la libreria

--- frontend/emi/cronjob/cronjob-detail/custom-validators.ts
+++ frontend/emi/cronjob/cronjob-detail/custom-validators.ts
@@ -4,23 +4,8 @@ import {
   FormGroup,
   ValidationErrors
 } from '@angular/forms';
-import { parseString } from 'cron-parser';
 
 export class CustomValidators {
-  static cronjobFormatValidate(cronjobFormat: FormControl): ValidationErrors {
-    try {
-      console.log('keys: ', parseString(cronjobFormat.value).errors);
-      if (!Object.keys(parseString(cronjobFormat.value).errors).length) {
-        return null;
-      }
-      throw 'Invalid CronjobFormat';
-    } catch (ex) {
-      console.log('excepcion en format: ', ex);
-      return {
-        cronjobFormatValidate: true
-      };
-    }
-  }
 
   static bodyFormatValidate(body: FormControl): ValidationErrors {
     const isValid = false;
